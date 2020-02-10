a2cd0e42f64370d4b0fc07895de5d52b5d921f28
Howard van Waard <thethirdnexusofalltime@gmail.com>
2020-02-10 02:32:05 +0000

    Start
    added field with different tiles
    added card class
    cards have threat and value, properites that the ai will assign to them
    added unit class
    units have damage and health, which will be used in calculating threat and value

---
+++ Assets/Card.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card : MonoBehaviour {
+    public float threat;
+    public float value;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Unit.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit : Card {
+    [SerializeField]
+    bool onField;
+    public Vector2 position;
+    public int damage;
+    public int health;
+	// Use this for initialization
+	void Start () {
+        value = damage * health;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
